12e1eee4800b08f8f604a19b412553b28b8c2b37
Brenda gutierrez murguia <Brenda gutierrez murguia>
2017-03-19 11:27:01 -0600

    agregar estilos.css,cambiar js a agregarlista.js,agregar carpteta con fuentes

---
+++ assets/js/agregarlista.js
@@ -0,0 +1,6 @@
+function agreagarLi(){
+    var texto=document.getElementById('liname').value;
+    var li=document.createElement('LI');
+    li.innerHTML=texto==''?'(nada)':texto;
+    document.getElementById('lista').appendChild(li);
+}
