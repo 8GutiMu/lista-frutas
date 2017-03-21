bfeb9c6f9f7a44bcb275b16b512c4542e508df1e
Brenda gutierrez murguia <Brenda gutierrez murguia>
2017-03-21 08:45:16 -0600

    cambios finales

--- assets/js/agregarlista.js
+++ assets/js/agregarlista.js
@@ -1,5 +1,5 @@
 function agreagarLi(){
-    var texto=document.getElementById('liname').value;
+    var texto=document.getElementById('linew').value;
     var li=document.createElement('LI');
     li.innerHTML=texto==''?'(nada)':texto;
     document.getElementById('lista').appendChild(li);
