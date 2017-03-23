b5706892dcbcb16847684473cfe92b3820f402a0
Brenda gutierrez murguia <Brenda gutierrez murguia>
2017-03-23 08:57:50 -0600

    eliminar elementos en index.html y alerts vacio

--- assets/js/agregarlista.js
+++ assets/js/agregarlista.js
@@ -1,6 +1,24 @@
 function agreagarLi(){
     var texto=document.getElementById('linew').value;
-    var li=document.createElement('LI');
-    li.innerHTML=texto==''?'(nada)':texto;
-    document.getElementById('lista').appendChild(li);
+
+    if (texto== ""){
+      alert("NO ESTAS AGREGANDO NADA!!!!!");
+    } else {
+      var li=document.createElement('LI');
+      li.innerHTML=texto;
+      document.getElementById('lista').appendChild(li);
+      document.getElementById("linew").value="";
+    }
+
+}
+
+function borrar(){
+    lis=document.getElementById('lista').getElementsByTagName('li');
+    for(var i=0; i<lis.length;i++){
+        lis[i].onclick=function(){
+            if(confirm('¿quieres borrar esta fruta???'))
+            this.parentNode.removeChild(this);
+        };
+    }
+
 }
